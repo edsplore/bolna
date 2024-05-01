12f916bfb2f9c7a095d4a1ddc8368e35808280e0
edsplore <138727039+edsplore@users.noreply.github.com>
2024-05-01 20:01:57 +0530

    test agent 3

--- initiate_call.py
+++ initiate_call.py
@@ -3,7 +3,7 @@ import requests
 url = "http://0.0.0.0:8001/call"
 
 payload = {
-    "agent_id": "ce5947da-3927-4b78-b3ce-1781719c8e17",
+    "agent_id": "876610d2-9187-4757-aa14-b717cd1c9fdc",
     "recipient_phone_number": "+917042423450",
     "user_data": {
         "first_name": "Anmol",
