00cfda066baf595773dd31fb33961682fcd58426
edsplore <138727039+edsplore@users.noreply.github.com>
2024-05-01 19:36:04 +0530

    test agent 2.3

--- initiate_call.py
+++ initiate_call.py
@@ -4,7 +4,7 @@ url = "http://0.0.0.0:8001/call"
 
 payload = {
     "agent_id": "ce5947da-3927-4b78-b3ce-1781719c8e17",
-    "recipient_phone_number": "+919811966113",
+    "recipient_phone_number": "+917042423450",
     "user_data": {
         "first_name": "Anmol",
         "last_name": "Rishi",
