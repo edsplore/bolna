2abae24a11c5bcbbc1fa290b2e74dd91253b3fa2
edsplore <138727039+edsplore@users.noreply.github.com>
2024-05-01 20:13:19 +0530

    updated agent 3.3

--- initiate_call.py
+++ initiate_call.py
@@ -4,7 +4,7 @@ url = "http://0.0.0.0:8001/call"
 
 payload = {
     "agent_id": "876610d2-9187-4757-aa14-b717cd1c9fdc",
-    "recipient_phone_number": "+919654115965",
+    "recipient_phone_number": "+917042423450",
     "user_data": {
         "first_name": "Anmol",
         "last_name": "Rishi",
