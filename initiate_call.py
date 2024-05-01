ed2f19c1fdd0db5e94612d7c7f73470599e49998
edsplore <138727039+edsplore@users.noreply.github.com>
2024-05-01 17:25:08 +0530

    changed number

--- initiate_call.py
+++ initiate_call.py
@@ -4,7 +4,7 @@ url = "http://0.0.0.0:8001/call"
 
 payload = {
     "agent_id": "ce5947da-3927-4b78-b3ce-1781719c8e17",
-    "recipient_phone_number": "+919654115965",
+    "recipient_phone_number": "+919811966113",
     "user_data": {
         "first_name": "Anmol",
         "last_name": "Rishi",
