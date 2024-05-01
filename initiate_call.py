889f4d79f0f238e696a3b2546c32214beea0180d
edsplore <138727039+edsplore@users.noreply.github.com>
2024-05-01 17:23:21 +0530

    added initiate call api call

---
+++ initiate_call.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "http://0.0.0.0:8001/call"
+
+payload = {
+    "agent_id": "ce5947da-3927-4b78-b3ce-1781719c8e17",
+    "recipient_phone_number": "+919654115965",
+    "user_data": {
+        "first_name": "Anmol",
+        "last_name": "Rishi",
+        "honorific": "Mr."
+    }
+}
+headers = {
+    #"Authorization": "Bearer <token>",
+    "Content-Type": "application/json"
+}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+print(response.text)
